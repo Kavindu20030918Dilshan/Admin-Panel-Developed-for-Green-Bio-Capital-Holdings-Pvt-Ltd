9e2db900c99ddde342329593e8187fc3b0882d44
Kavindu Dilshan <141320409+Kavindu20030918Dilshan@users.noreply.github.com>
2024-07-22 14:43:35 +0530

    Update adminVerificationProcess.php

--- adminVerificationProcess.php
+++ adminVerificationProcess.php
@@ -27,13 +27,13 @@ if ($num > 0) {
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-        $mail->Username = 'dilshan58762@gmail.com';                     //SMTP username
-        $mail->Password = 'uwcxdtmkyevwvykt';                               //SMTP password
+        $mail->Username = '#email';                     //SMTP username
+        $mail->Password = '#app password';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('dilshan58762@gmail.com', 'Green Bio Capital Holdings');
+        $mail->setFrom('#email', 'Green Bio Capital Holdings');
         $mail->addAddress($email);     //Add a recipient
         $mail->addReplyTo('info@example.com', 'Information');
 
